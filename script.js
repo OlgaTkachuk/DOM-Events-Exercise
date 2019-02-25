06bddeb476b687afe504e27694a7732d280a2e03
OlgaTkachuk <tkachukk.olga@gmail.com>
2019-02-25 12:34:13 +0200

    ES5/ES6 issue fixed

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-let button = document.getElementById("enter");
-let input = document.getElementById("userinput");
-let ul = document.querySelector('ul');
-// let listItems = document.getElementsByTagName('li');
-// console.log(listItems);
+const button = document.getElementById("enter");
+const input = document.getElementById("userinput");
+const ul = document.querySelector('ul');
 
 function inputLength() {
     return input.value.length;
 }
 
 function createListElement() {
+
     let li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
     input.value = "";
 
+
     li.addEventListener("click", function() {
         let done = this.classList.toggle("done");
-        var deleteBtn = document.createElement("button");
+        let deleteBtn = document.createElement("button");
         deleteBtn.classList.add("deleteButton");
 
         if (done) {
@@ -50,6 +50,3 @@ function addListAfterClick() {
 
 button.addEventListener('click', addListAfterClick);
 input.addEventListener('keypress', addListAfterKeypress);
-// for (let i = 0 ; i < list_items.length; i++) {
-//     list_items[i].addEventListener('click' , addDoneClass ) ;
-// }
